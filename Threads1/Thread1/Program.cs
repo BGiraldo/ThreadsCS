28b876cd3e1b57de09a36d0caab12c392870a706
BGiraldo <bgiraldox@gmail.com>
2017-06-23 13:26:29 -0500

    Threads in c#

---
+++ Threads1/Thread1/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// Para usar Hilos
+using System.Threading;
+
+namespace Thread1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Thread hilo = new Thread(pintarCaracter);
+            hilo.Start();
+            // hilo.Join();
+            Thread.Sleep(2000);
+            pintarY();
+            Thread.Sleep(2000);
+
+            Thread hilo2 = new Thread(() => pintarPalabras(".NET","C#"));
+            hilo2.Start();
+
+
+            // para que la consola espere y no se cierre
+            Console.Read();
+
+        }
+
+
+        public static void pintarCaracter()
+        {
+
+            for (int i = 0; i < 1000 ; i++)
+            {
+                Console.WriteLine("X"+i);
+            }
+
+        }
+
+        public static void pintarY()
+        {
+            for (int i = 0; i < 1000; i++)
+            {
+                Console.WriteLine("Y"+i);
+            }
+        }
+
+        public static void pintarPalabras(String p1, String p2)
+        {
+            for (int i = 0; i < 1000; i++)
+            {
+                Console.WriteLine(p1 + " " + p2);
+            }
+        }
+
+    }
+}
---
+++ Threads1/ThreadWithUI/Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+// Using Necessary
+using System.Threading;
+
+namespace ThreadWithUI
+{
+    public partial class Form1 : Form
+    {
+
+        Thread hiloRojo;
+        Thread hiloAzul;
+        Random aleatorio;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            aleatorio = new Random();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            hiloRojo = new Thread(pintarRojo);
+            hiloRojo.Start();
+        }
+
+        public void pintarRojo()
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                this.CreateGraphics().
+                    DrawEllipse(new Pen(Color.Red, 4),
+                    aleatorio.Next(0, this.Width),
+                    aleatorio.Next(0, this.Height), 20, 20);
+                Thread.Sleep(200);
+            }
+        }
+
+        public void pintarAzul()
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                this.CreateGraphics().
+                    DrawEllipse(new Pen(Color.Blue, 4),
+                    aleatorio.Next(0, this.Width),
+                    aleatorio.Next(0, this.Height), 20, 20);
+                Thread.Sleep(200);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            hiloAzul = new Thread(pintarAzul);
+            hiloAzul.Start();
+        }
+    }
+}
